0f1e9fca68ea11728ae3aae07254b83077984b6f
XihengY <92489126+XihengY@users.noreply.github.com>
2021-12-03 00:16:12 -0800

    Add files via upload

--- binary_tree.py
+++ binary_tree.py
@@ -20,13 +20,13 @@ class BinaryTreeTopo( Topo ):
         h8 = self.addHost('h8')
 
         # Add switches
-        s1 = self.addHost('s1')
-        s2 = self.addHost('s2')
-        s3 = self.addHost('s3')
-        s4 = self.addHost('s4')
-        s5 = self.addHost('s5')
-        s6 = self.addHost('s6')
-        s7 = self.addHost('s7')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+        s5 = self.addSwitch('s5')
+        s6 = self.addSwitch('s6')
+        s7 = self.addSwitch('s7')
 
         # Add links
         self.addLink(s1, s2)
